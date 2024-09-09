4eade6635e94a075c588c72645482a2fbd6afd75
tatyershova <taty.ershova@gmail.com>
2024-09-10 01:07:42 +0300

    add Pool to Pinger

--- projects/pinger/pinger.py
+++ projects/pinger/pinger.py
@@ -4,11 +4,11 @@ import re
 import random
 import ipaddress
 import sys
-
+from multiprocessing import Pool
 
 
 def ping(ip_address):
-    ping_result = subprocess.run(['ping', '-c','4',str(ip_address)],text=True, capture_output=True)
+    ping_result = subprocess.run(['ping', '-c','1',str(ip_address)],text=True, capture_output=True)
     print(ping_result.stdout)
     return(ping_result.stdout)
 
@@ -35,21 +35,25 @@ def readfile(filename):
     return ip_addresses
 
 def randomgeneratorip(networks,limit,file):
-    i = 1
-    while i <= limit:
+    random_ips = []
+    for _ in range(limit):
         random_network = random.choice(ip_addresses)
         network = ipaddress.ip_network(random_network)
         all_ip = list(network.hosts())
         random_ip = random.choice(all_ip)
-        print(random_ip)
-        i +=1
-        ping_zero = ping(random_ip)
-        newvar = parseping(ping_zero)
-        file.write(str(random_ip) +" " + newvar[0] +" " + newvar[1] + "\n" )
-    return ping_zero
+        #print(random_ip)
+        random_ips.append(random_ip)
+    print(random_ips)
+    with Pool(limit) as p:
+        results = p.map(ping, random_ips)
+    for i in range(limit):
+        newvar = parseping(results[i])
+        print(parseping(str(results[i])))
+        file.write(str(random_ips[i]) +" " + newvar[0] +" " + newvar[1] + "\n" )
+    #return ping_zero
 if __name__ == '__main__':
     ip_addresses = readfile(sys.argv[1])
     with open ('ping_result.txt', 'w') as f:
-        ping_zero = randomgeneratorip(ip_addresses, int(sys.argv[2]),f)
+        results = randomgeneratorip(ip_addresses, int(sys.argv[2]),f)
     
     
